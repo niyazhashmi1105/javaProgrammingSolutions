1ce624b679188ba97aa0654601f359d3c779eb22
MD Niyaz Hashmi <mdniyazhashmi@example.com>
2025-09-27 19:37:17 +0530

    reverse linked list

--- src/main/java/com/java/datastructure/LinkedList.java
+++ src/main/java/com/java/datastructure/LinkedList.java
@@ -17,7 +17,10 @@ public class LinkedList {
         LinkedList head = new LinkedList(10);
         head.next = new LinkedList(20);
         head.next.next = new LinkedList(30);
+        head.next.next.next = new LinkedList(40);
         traverseLinkedList(head);
+        System.out.println();
+        traverseLinkedList(reverseLinkedList(head));
     }
 
     public static void traverseLinkedList(LinkedList head){
@@ -26,5 +29,20 @@ public class LinkedList {
             System.out.print(current.getData()+" ");
             current = current.next;
         }
+    }
+
+    public static LinkedList reverseLinkedList(LinkedList head){
+        LinkedList current = head;
+        LinkedList prev = null;
+        LinkedList next = null;
+
+        while(current != null){
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+
+        }
+        return prev;
     }
 }
